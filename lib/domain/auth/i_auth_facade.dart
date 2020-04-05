7f35483bbc8211e51edb826b37d4b5efddc8cce5
SpaGettys <jgettys@gmail.com>
2020-04-06 00:24:39 +0900

    Video 3 https://www.youtube.com/watch?v=jNF48xIfnq8&feature=youtu.be Flutter Firebase & DDD Course [3] – Auth Facade Interface
    flutter pub run build_runner watch --delete-conflicting-outputs
    
    Notes:
    We are still not messing with firebase directly.
    We will just create a nice safe layer (Auth Facade Interface) between firebase and our domain layer.
    Point: We don't want our app to be entanged with FireBase.
    
    Backend Authenticaition Actions: FirebaseAuth & GoogleSignIn
    
    The things we eventually need to be able to do:
    1) Register with email and password
    2) Sign in with email and password
    3) Sign in with Google
    
    The facade design pattern: Take multiple classes with weird interfaces and organize them into a nice unified ainterface.

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,21 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+import 'package:notes_firebase_ddd_course/domain/auth/auth_failure.dart';
+
+import 'value_objects.dart';
+
+// Point of making IAuthFacade↓
+// We are simplified the interfaces of the below two classes:
+// FirebaseAuth, GoogleSignIn
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
